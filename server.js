83863a12fa70fa393f9d93dea12109bad278aa05
Regina Lee <regina.v.lee@gmail.com>
2017-03-01 19:17:05 -0800

    working on put requests to edit classes

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-var express = require('express');
+const express = require('express');
+const app = express();
+const router = require('./server/routes');
+const bodyParser = require('body-parser');
+
+const port = process.env.PORT || 8080;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', router);
+
+app.listen(port, 'localhost', () => {
+  console.log("We are listening on: ", port);
+})
+
+
